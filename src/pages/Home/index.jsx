e3419f76f25bc240d64a07ca199ac3acd6b29fe4
rishavvrathod <96969922+rishavvrathod@users.noreply.github.com>
2023-08-29 21:24:59 +0530

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,54 @@
-import React,{useState,useCallback} from "react";
+import React, { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
-const HomePage = () =>{
-    const [value,setValue] = useState('');
-    const navigate = useNavigate() ;
+const HomePage = () => {
+  const [value, setValue] = useState("");
+  const navigate = useNavigate();
 
-    const handleRoomJoin = useCallback(() =>{
-        navigate(`/room/${value}`)
-     },[navigate,value]);
+  const handleRoomJoin = useCallback(() => {
+    navigate(`/room/${value}`);
+  }, [navigate, value]);
 
-    return (
-        <div>
-            <input value={value} 
-            onChange={(e) => setValue(e.target.value)}
-            type="text"
-            placeholder="Enter Room Id"
-            />
-            <button
-            onClick={handleRoomJoin}
-            >Submit</button>
-        </div>
-    )
-}
+  return (
+    <div style={styles.container}>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        placeholder="Enter Room Id"
+        style={styles.input}
+      />
+      <button onClick={handleRoomJoin} style={styles.button}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
-export default HomePage ;
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  },
+  input: {
+    padding: "10px",
+    marginBottom: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    fontSize: "16px",
+  },
+  button: {
+    padding: "10px 20px",
+    background: "#007bff",
+    color: "white",
+    border: "none",
+    borderRadius: "4px",
+    fontSize: "16px",
+    cursor: "pointer",
+  },
+};
+
+export default HomePage;
